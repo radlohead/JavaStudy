f7dec59bea747417a08ce38dd8a5c4ab6054afb4
박민호 <radlohead@naver.com>
2018-12-02 17:04:36 +0900

    final example

---
+++ JavaStudy2/src/Final/Class.java
@@ -0,0 +1,16 @@
+package Final;
+
+final class A {
+	final void b() {
+		
+	}
+}
+
+// final은 불변을 의미한다. 클래스는 상속 변수는 재대입이 불가하다.
+//public class Class extends A {
+//
+//	public static void main(String[] args) {
+//
+//	}
+//
+//}
---
+++ JavaStudy2/src/Final/Variable.java
@@ -0,0 +1,10 @@
+package Final;
+
+public class Variable {
+	static final int num = 10;
+	
+	public static void main(String[] args) {
+//		num = 20;
+	}
+}
+
